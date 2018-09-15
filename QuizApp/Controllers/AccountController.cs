1f160b8a8406845c0dfa9a4de452e2a1951e0aae
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-09-15 13:07:00 +0200

    added AccountC, SignInVM, SignIn view with _account layout

---
+++ QuizApp/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuizApp.Context;
+using QuizApp.Models;
+using QuizApp.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuizApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        protected UserManager<AppUser> UserManager { get; }
+        protected SignInManager<AppUser> SignInManager { get; }
+        protected RoleManager<IdentityRole<int>> RoleManager;
+        protected  AppDbContext Context { get; }
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole<int>> roleManager, AppDbContext context)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            RoleManager = roleManager;
+            Context = context;
+        }
+        
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel signInViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await SignInManager.PasswordSignInAsync(signInViewModel.Login, signInViewModel.Password,
+                    signInViewModel.RememberMe, false);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("", "Wrong login or password");
+            }
+
+            return View(signInViewModel);
+        }
+    }
+}
